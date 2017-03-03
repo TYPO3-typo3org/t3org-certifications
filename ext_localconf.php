1bfab59d4f0c68dcdf918099d118ddca49edff94
Andreas Wolf <dev@a-w.io>
2017-03-03 17:13:42 +0100

    [BUGFIX] Missing plugin vendor namespace

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    $_EXTKEY,
+    'T3o.' . $_EXTKEY,
     'Certlist',
     [
         'Listing' => 'list, show',
