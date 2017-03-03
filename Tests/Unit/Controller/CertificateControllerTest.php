21fb1e07613007a3f8bf7fdb243b97361209a78f
Andreas Wolf <dev@a-w.io>
2017-03-03 16:06:20 +0100

    [TASK] Update copyright notices

--- Tests/Unit/Controller/CertificateControllerTest.php
+++ Tests/Unit/Controller/CertificateControllerTest.php
@@ -1,40 +1,15 @@
 <?php
-
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2014 Ole Hartwig <o.hartwig@web-vision.de>, web-vision gmbh
- *  			
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 /**
- * Test case for class Tx_Certifications_Controller_CertificateController.
+ * This file is part of the TYPO3 CMS project.
  *
- * @version $Id$
- * @copyright Copyright belongs to the respective authors
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- * @package TYPO3
- * @subpackage Certifications
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- * @author Ole Hartwig <o.hartwig@web-vision.de>
+ * The TYPO3 project - inspiring people to share!
  */
 class Tx_Certifications_Controller_CertificateControllerTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
 	/**
